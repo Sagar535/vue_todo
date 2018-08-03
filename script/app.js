cfeea93d7d4efdfd048575f2a986a83c7f70a0fa
Sagar Shah <070bct535@ioe.edu.np>
2018-08-03 07:27:00 +0545

    Simplest form of todo app created

--- script/app.js
+++ script/app.js
@@ -3,16 +3,23 @@ var todo_app = new Vue({
 
 	data: {
 		new_task: '',
-		task_list: []
+		task_list: [],
+		completed_list: []
 	},
 
 	methods: {
 		addTask: function() {
 
 			if (this.new_task.trim().length > 0){
-				this.task_list.push({text:this.new_task});
+				this.task_list.push({text:this.new_task, done: false});
 				this.new_task = '';
 			}	
-		}
+		},
+
+		removeTask: function(index) {
+			this.task_list[index].done=true;
+			this.completed_list.push(this.task_list[index]);
+			this.task_list.splice(index,1);
+		} 
 	}
 });
